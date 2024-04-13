da5d5daf60e2fc671787b91898c4ff891f202185
napasorn <napasorn.tsp@gmail.com>
2024-04-13 18:42:46 +0700

    submit: JavaScript Computational Thinking Assignment

--- ex-3.js
+++ ex-3.js
@@ -82,3 +82,11 @@ let orders = [
 ];
 
 // Start coding here
+let birdieSheplandpaid = 0;
+for (let i = 0; i < orders.length; i++) {
+  if (orders[i].customerName === "Birdie Shepland"){
+     birdieSheplandpaid = birdieSheplandpaid + orders[i].productPrice * orders[i].productQuantity;
+  }
+}
+
+console.log("Total paid amount of Birdie Shepland: " + birdieSheplandpaid + " Baht")
